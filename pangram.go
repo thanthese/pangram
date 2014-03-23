4374c282a0d50ff62cb8af797fbcd68c823d5b1f
Stephen Mann <thanthese@gmail.com>
2014-03-23 02:41:00 -0500

    solid results

--- pangram.go
+++ pangram.go
@@ -3,23 +3,57 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
-var threshold = 24
+// todo
+//
+// - There seems to be a bug, or at least something I don't understand, where
+// sets of words are printed twice.
+//
+// - I don't like how recur() prints. It would be better if it returned its
+// results (preferrably via channel, so the user can still watch results come
+// in in real time) and the caller dealt with printing.
+//
+// - It would be nice if recur was executing in multiple goroutines in
+// parallel, for speed and whatnot.
+//
+// - None of my results contain a 'q' or 'z'. Maybe I can *force* the first two
+// words to have those letters. Then I could use a larger dictionary, but skip
+// the cost of a ton of computation.
 
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var threshold int
+
+// we'll fake a set with a map
 type set map[rune]bool
 
+func usage() {
+	fmt.Println("pangram <threshold> <dict path>")
+	fmt.Println()
+	fmt.Println("- threshold: display words if they use >= threshold unique letters")
+	fmt.Println("- dict path: path to list of words. One word per line, lower case, a-z only.")
+	os.Exit(1)
+}
+
 func main() {
-	if len(os.Args) < 1 {
-		log.Fatal("pangram requires path argument")
+	if len(os.Args) < 2 {
+		usage()
 	}
-	path := os.Args[1]
+
+	th, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err.Error())
+		usage()
+	}
+	threshold = th
+
 	fmt.Println("Loading word list...")
+	path := os.Args[2]
 	wordlist := loadWordList(path)
 
 	fmt.Println("Building anagrams list...")
@@ -47,7 +81,8 @@ func mapKeys(m map[string][]string) []string {
 func loadWordList(path string) []string {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
+		usage()
 	}
 	return strings.Split(string(content), "\n")
 }
@@ -74,6 +109,8 @@ func sortWord(word string) string {
 	return strings.Join(runes, "")
 }
 
+// Filter wordlist so that no words contain a letter more than once. For
+// example, "house" would be allowed, but "example" would be filtered out.
 func removeDoubles(wordList []string) []string {
 	var ret []string
 	for _, word := range wordList {
@@ -126,18 +163,19 @@ func recur(used set, foundwords []string, potentials []string, anagrams map[stri
 		fw := append(foundwords, word)
 
 		// prepare (filter) new potentials
-		rm := make([]string, 0, len(potentials))
+		ps := make([]string, 0, len(potentials))
 		for j := i + 1; j < len(potentials); j++ {
 			w := potentials[j]
 			if len(w)+len(u) <= 26 && wordFits(u, w) {
-				rm = append(rm, w)
+				ps = append(ps, w)
 			}
 		}
 
-		recur(u, fw, rm, anagrams)
+		recur(u, fw, ps, anagrams)
 	}
 }
 
+// Return the sum of lengths of all words in the list.
 func runesCount(words []string) int {
 	sum := 0
 	for _, word := range words {
@@ -146,6 +184,9 @@ func runesCount(words []string) int {
 	return sum
 }
 
+// Return true if the word does not contain any letters that have already been
+// checked off by the used set. That is, the word only contains still allowable
+// letters.
 func wordFits(used set, word string) bool {
 	for _, c := range word {
 		if used[c] {
---
+++ readme.md
@@ -0,0 +1,38 @@
+Find a [panagram][pan], but with two differences:
+
+1. Use each letter *exactly* once, instead of *at least* once.
+
+2. No grammar restrictions -- it doesn't need to be a sentence.
+
+[pan]: http://en.wikipedia.org/wiki/Panagram
+
+## Motivation
+
+I was playing with my infant son's blocks. He has 26 of them, one for each
+letter. I got to wondering how many words I could spell with those blocks.
+Could I use them all?
+
+## Thoughts on dictionary
+
+After looking at some perfect pangrams online it became clear that I didn't want to use a full dictionary. [For example][w]:
+
+    Cwm fjord veg balks nth pyx quiz.
+
+[w]: http://en.wikipedia.org/wiki/List_of_pangrams
+
+Sure it's a perfect pangram, but it won't impress anybody because it looks like a monkey typed it out.
+
+My solution was to restrict my search to commonly used words.
+
+## Results
+
+My best results after using the 10,000 most commonly used words found in all [Project Gutenberg][pg] books:
+
+[pg]: http://www.gutenberg.org/
+
+    24 [dry] [length] [fix] [backs] [jump] [vow]
+    24 [things nights] [verb] [wax] [dock] [fly] [jump]
+    24 [why] [bricks] [fox] [vent] [glad] [jump]
+    24 [why] [drag] [blocks] [fix] [vent] [jump]
+    24 [why] [grand] [block] [vest] [fix] [jump]
+    24 [why] [grind] [fox] [vest] [black] [jump]
