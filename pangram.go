8d7249f2390ffd5b4b32263d7945e89ceadf2cbb
Stephen Mann <thanthese@gmail.com>
2014-03-22 19:44:49 -0500

    works with prettier results, faster

--- pangram.go
+++ pangram.go
@@ -4,23 +4,44 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"sort"
 	"strings"
 )
 
 var alphabet = "abcdefghijklmnopqrstuvwxyz"
-var threshold = 21
+var threshold = 24
 
 type set map[rune]bool
 
 func main() {
-	wordlist := loadWordList("/Users/thanthese/pangram/simplewords.txt")
+	if len(os.Args) < 1 {
+		log.Fatal("pangram requires path argument")
+	}
+	path := os.Args[1]
+	fmt.Println("Loading word list...")
+	wordlist := loadWordList(path)
+
+	fmt.Println("Building anagrams list...")
 	singlesOnly := removeDoubles(wordlist)
 	anagrams := buildAnagrams(singlesOnly)
-	_ = anagrams
+	words := mapKeys(anagrams)
+	fmt.Println("- ", len(wordlist), "list length")
+	fmt.Println("- ", len(singlesOnly), "with unique letters")
+	fmt.Println("- ", len(anagrams), "anagrams")
+
+	fmt.Println("Finding pangrams...")
 	used := set{}
 	found := []string{}
-	recur(used, found, singlesOnly)
+	recur(used, found, words, anagrams)
+}
+
+func mapKeys(m map[string][]string) []string {
+	ret := make([]string, 0, len(m))
+	for k, _ := range m {
+		ret = append(ret, k)
+	}
+	return ret
 }
 
 func loadWordList(path string) []string {
@@ -75,25 +96,45 @@ func containsDoubles(word string) bool {
 	return false
 }
 
-func recur(used set, foundwords []string, remainingwords []string) {
-	if len(used) >= threshold {
-		fmt.Println(runesCount(foundwords), foundwords)
-		return
-	}
-
-	if len(remainingwords) == 0 {
-		return
-	}
-
-	for i, word := range remainingwords {
-		if isValidAddition(used, word) {
-			u := copymap(used)
-			for _, c := range word {
-				u[c] = true
-			}
-			fw := append(foundwords, word)
-			recur(u, fw, remainingwords[i+1:])
+func prettyFinding(words []string, anagrams map[string][]string) {
+	fmt.Print(runesCount(words))
+	for _, w := range words {
+		if len(w) > 0 {
+			fmt.Print(" ", anagrams[w])
 		}
+	}
+	fmt.Println()
+}
+
+func recur(used set, foundwords []string, potentials []string, anagrams map[string][]string) {
+	if len(used) == 26 || len(potentials) == 0 {
+		if len(used) >= threshold {
+			prettyFinding(foundwords, anagrams)
+		}
+		return
+	}
+
+	for i, word := range potentials {
+
+		// prepare new set
+		u := copymap(used)
+		for _, r := range word {
+			u[r] = true
+		}
+
+		// prepare new foundwords
+		fw := append(foundwords, word)
+
+		// prepare (filter) new potentials
+		rm := make([]string, 0, len(potentials))
+		for j := i + 1; j < len(potentials); j++ {
+			w := potentials[j]
+			if len(w)+len(u) <= 26 && wordFits(u, w) {
+				rm = append(rm, w)
+			}
+		}
+
+		recur(u, fw, rm, anagrams)
 	}
 }
 
@@ -105,7 +146,7 @@ func runesCount(words []string) int {
 	return sum
 }
 
-func isValidAddition(used set, word string) bool {
+func wordFits(used set, word string) bool {
 	for _, c := range word {
 		if used[c] {
 			return false
